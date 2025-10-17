bff576f8e736698cd2fcf167301aadfad38e3ffc
hansikaagrawal930-jpg <hansikaagrawal930@gmail.com>
2025-10-17 08:58:08 +0530

    Add files via upload

---
+++ Number1.java
@@ -0,0 +1,8 @@
+public class Number1 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        int c = a + b;
+        System.out.println("sum:" + c);
+    }
+}
