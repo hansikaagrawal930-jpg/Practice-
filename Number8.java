843610076a2e607986c3ff67f876d015ec6a922b
hansikaagrawal930-jpg <hansikaagrawal930@gmail.com>
2025-10-19 11:22:16 +0530

    Add files via upload

---
+++ Number8.java
@@ -0,0 +1,7 @@
+public class Number8 {
+    public static void main(String[] args) {
+        for(int i=10; i>0 ; i=i-2){
+            System.out.println("hello world");
+        }
+    }
+}
---
+++ Number9.java
@@ -0,0 +1,9 @@
+public class Number9 {
+    public static void main(String[] args) {
+        int i = 1;                    // initialization
+        while (i <= 5) {              // condition
+            System.out.println("Hello Hansika");
+            i++;                      // update
+        }
+    }
+}
